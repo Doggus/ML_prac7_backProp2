c918a3432db49ae63c5f2d67fd18be75508ddb36
Doggus <ldoggy20@gmail.com>
2018-09-26 16:40:07 +0200

    Added bias nodes to the neural network and fixed a minor mistake, not getting desired value with one back prop, going to attempt to do multiple forward passes and back props to make network learn the correct weights

--- main.cpp
+++ main.cpp
@@ -93,24 +93,33 @@ int main()
    //Weights are intialised to random float betwen -1 and 1 (they are not known in the beginning)
    //(initial weight doesn't matter - it will change as program finds the true weight given the desired output)
 
+   //According to sasha, you should probably always have a bias node unless instructed otherwise
+   //when back propogating i dont think initial weight bias means that much, because it gets updated later anyway 
+   //But if just forward passing, I can see how that would effect the neural network 
+
+   bool complete = false;
+
    //FORWARD PASS:
+   cout << "FORWARD PASS" << endl;
+   cout << "" << endl;
 
    vector<neuron> inputLayer = 
 	{
 		neuron(XORdata[0].input1, vector<float> {getRandomFloat(-1,1), getRandomFloat(-1,1), getRandomFloat(-1,1)}),
-		neuron(XORdata[0].input1, vector<float> {getRandomFloat(-1,1), getRandomFloat(-1,1), getRandomFloat(-1,1)}),
-		neuron(XORdata[0].input1, vector<float> {getRandomFloat(-1,1), getRandomFloat(-1,1), getRandomFloat(-1,1)})
-		//Bias neuron neccesary??
+		neuron(XORdata[0].input2, vector<float> {getRandomFloat(-1,1), getRandomFloat(-1,1), getRandomFloat(-1,1)}),
+		neuron(XORdata[0].input3, vector<float> {getRandomFloat(-1,1), getRandomFloat(-1,1), getRandomFloat(-1,1)}),
+		//Bias neuron: always has node value of 1, its weights are the bias (they are manually chosen)
+		neuron(1.0f, vector<float> {0.1f, -0.2, 0.3f})
 	};
 
-	//vectors of all input neuron float values and the their weights that point to each hidden neuron
-	vector<float> inputValues {inputLayer[0].nodeValue, inputLayer[1].nodeValue, inputLayer[2].nodeValue};
+	//vectors of all input neuron float values and the their weights that point to each hidden neuron (and the bias)
+	vector<float> inputValues {inputLayer[0].nodeValue, inputLayer[1].nodeValue, inputLayer[2].nodeValue, inputLayer[3].nodeValue};
 	//weights pointing to hidden node 1
-	vector<float> weights1 {inputLayer[0].weights[0], inputLayer[1].weights[0], inputLayer[2].weights[0]};
+	vector<float> weights1 {inputLayer[0].weights[0], inputLayer[1].weights[0], inputLayer[2].weights[0], inputLayer[3].weights[0]};
 	//weights pointing to hidden node 2
-	vector<float> weights2 {inputLayer[0].weights[1], inputLayer[1].weights[1], inputLayer[2].weights[1]};
+	vector<float> weights2 {inputLayer[0].weights[1], inputLayer[1].weights[1], inputLayer[2].weights[1], inputLayer[3].weights[1]};
 	//weights pointing to hidden node 3
-	vector<float> weights3 {inputLayer[0].weights[2], inputLayer[1].weights[2], inputLayer[2].weights[2]};
+	vector<float> weights3 {inputLayer[0].weights[2], inputLayer[1].weights[2], inputLayer[2].weights[2], inputLayer[3].weights[2]};
 
 	//default node value to 0 before we calculate its value (initial value is arbitrary), weight once again initialised to random float
 	//Neural network converges to one output from the hidden layer, thus hidden nodes will have one weight pointing to the out put node
@@ -126,18 +135,22 @@ int main()
 	//calculate float values for hidden node 3
 	hiddenNeuron3.calculateLayerValue(inputValues, weights3);
 
+	//hidden bias node (manually assigned a weight) [always has node value of 1]
+	neuron hiddenBias = neuron(1.0f, vector<float> {0.1f});
+
 	cout << "Forward pass: HiddenNeuron1: " << hiddenNeuron1.nodeValue << endl;
 	cout << "Forward pass: HiddenNeuron2: " << hiddenNeuron2.nodeValue << endl;
 	cout << "Forward pass: HiddenNeuron3: " << hiddenNeuron3.nodeValue << endl;
+	cout << "Forward pass: HiddenBias: " << hiddenBias.nodeValue << endl;
 	cout << "" << endl;
 
 	//instantiate output empty neuron 
 	neuron outputNeuron = neuron(0, vector<float> {});
 
 	//vectors of all hidden neuron float values and the their weights that point to the output neuron
-	vector<float> hiddenValues {hiddenNeuron1.nodeValue, hiddenNeuron2.nodeValue, hiddenNeuron3.nodeValue};
+	vector<float> hiddenValues {hiddenNeuron1.nodeValue, hiddenNeuron2.nodeValue, hiddenNeuron3.nodeValue, hiddenBias.nodeValue};
 	//weights that point to the output node 
-	vector<float> hiddenWeights {hiddenNeuron1.weights[0], hiddenNeuron2.weights[0], hiddenNeuron3.weights[0]};
+	vector<float> hiddenWeights {hiddenNeuron1.weights[0], hiddenNeuron2.weights[0], hiddenNeuron3.weights[0], hiddenBias.weights[0]};
 
 	//calculate float values for output node
 	outputNeuron.calculateLayerValue(hiddenValues, hiddenWeights);
@@ -151,32 +164,40 @@ int main()
 	float learningRate = 0.1;
 
 	//BACK PROPOGATION:
+	cout << "BACK PROPOGATION" << endl;
+    cout << "" << endl;
 
 	//calculate output error values for back propagation
 	float outputError = outputNeuron.nodeValue*(1-outputNeuron.nodeValue)*(targetOutput-outputNeuron.nodeValue);
 
 	cout << "outputError: " << outputError << endl;
+	cout << "" << endl;
 
 	//Work out new weights in layer 2 (between hidden and output node):
 
 	hiddenWeights[0] += learningRate*outputError*hiddenNeuron1.nodeValue;
 	hiddenWeights[1] += learningRate*outputError*hiddenNeuron2.nodeValue;
 	hiddenWeights[2] += learningRate*outputError*hiddenNeuron3.nodeValue;
+	//update weights of the hidden bias node
+	hiddenWeights[3] += learningRate*outputError*hiddenBias.nodeValue;
 
 	cout << "New layer 2 weights (learning rate(n) = 0.1): " << endl;
 	cout << "H1: W: " << hiddenWeights[0] << endl;
 	cout << "H2: W: " << hiddenWeights[1] << endl;
 	cout << "H3: W: " << hiddenWeights[2] << endl;
+	cout << "HBias: W: " << hiddenWeights[3] << endl;
 	cout << "" << endl;
 
 	//calculate hidden error values for back propagation
 	float hiddenError1 = hiddenNeuron1.nodeValue*(1-hiddenNeuron1.nodeValue)*( (hiddenWeights[0]*outputError) );
 	float hiddenError2 = hiddenNeuron2.nodeValue*(1-hiddenNeuron2.nodeValue)*( (hiddenWeights[1]*outputError) );
 	float hiddenError3 = hiddenNeuron3.nodeValue*(1-hiddenNeuron3.nodeValue)*( (hiddenWeights[2]*outputError) );
+	float hiddenBiasError = hiddenBias.nodeValue*(1-hiddenBias.nodeValue)*( (hiddenWeights[3]*outputError) );
 
 	cout << "hiddenError1: " << hiddenError1 << endl;
 	cout << "hiddenError2: " << hiddenError2 << endl;
 	cout << "hiddenError3: " << hiddenError3 << endl;
+	cout << "hiddenBiasError: " << hiddenBiasError << endl;
 	cout << "" << endl;
 
 	//Now that we have the hidden node error values we work out the new weights in the first layer (between input and hidden nodes):
@@ -196,6 +217,11 @@ int main()
 	weights3[1] += learningRate*hiddenError3*inputValues[1];
 	weights3[2] += learningRate*hiddenError3*inputValues[2];
 
+	//update weights of bias node in the input layer
+	weights1[3] += learningRate*hiddenBiasError*inputValues[3]; // inputValues[3] = 1
+	weights2[3] += learningRate*hiddenBiasError*inputValues[3];
+	weights3[3] += learningRate*hiddenBiasError*inputValues[3];
+
 	cout << "New layer 1 weights (learning rate(n) = 0.1): " << endl;
 	cout << "I1: W1: " << weights1[0] << endl;
 	cout << "I1: W2: " << weights2[0] << endl;
@@ -208,6 +234,10 @@ int main()
 	cout << "I3: W1: " << weights1[2] << endl;
 	cout << "I3: W2: " << weights2[2] << endl;
 	cout << "I3: W3: " << weights3[2] << endl;
+	cout << "" << endl;
+	cout << "Ibias: W1: " << weights1[3] << endl;
+	cout << "Ibias: W2: " << weights2[3] << endl;
+	cout << "Ibias: W3: " << weights3[3] << endl;
 	cout << "" << endl;
 
 	//The mean-squared error should be 0 or very close to 0 to conclude that the network has learnt the XOR function correctly
